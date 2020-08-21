06b9b9c0f0b9c1eb8f93bee6bcb60a868d3d7cad
fyui001 <yun.cod@gmail.com>
2020-08-21 17:37:51 +0900

    eslintの設定を変えた

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
   parser: 'vue-eslint-parser',
+  // Settings for TypeScript linting
   parserOptions: {
-    parser: [
-      '@typescript-eslint/parser',
-      'babel-eslint',
-    ],
-    sourceType: "module",
-    ecmaVersion: 2016,
+    parser: '@typescript-eslint/parser'
   },
   env: {
     browser: true,
     jest: true,
-    es6: true
   },
   extends: [
-    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/base',
     'plugin:vue/strongly-recommended',
@@ -31,13 +27,13 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-return-await': 'off',
-    'no-console': 'off',
-    '@typescript-eslint/member-delimiter-style': 'off',
-    '@typescript-eslint/explicit-function-return-type' : 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+    '@typescript-eslint/member-delimiter-style': 'off'
   }
 }
